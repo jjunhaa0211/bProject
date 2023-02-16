cc4283397bc6c538b2385afe8252400103b63691
junha <goodjunha@gmail.com>
2023-02-16 18:46:31 +0900

    뺄셈연산 테스트 코드 수행

--- Calculator/src/main/java/org/example/Calculator.java
+++ Calculator/src/main/java/org/example/Calculator.java
@@ -2,6 +2,11 @@ package org.example;
 
 public class Calculator {
     public static int calculate(int operand1, String operand, int operand2) {
-        return operand1 + operand2;
+        if("+".equals(operand)) {
+            return operand1 + operand2;
+        } else if ("-".equals(operand)) {
+            return operand1 - operand2;
+        }
+        return 0;
     }
 }
