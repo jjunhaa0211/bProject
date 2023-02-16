1d670b346424605e8f8963746a2f046b262b68cb
junha <goodjunha@gmail.com>
2023-02-16 19:25:44 +0900

    코드 효율적이게 성공

---
+++ Calculator/src/main/java/org/example/ArithmeticOperator.java
@@ -0,0 +1,45 @@
+package org.example;
+
+import java.util.Arrays;
+
+public enum ArithmeticOperator {
+    ADDIRION("+") {
+
+        @Override
+        public int arithemticCalculate(int operand1, int operand2) {
+            return operand1 + operand2;
+        }
+    }, SUBTRACTION("-") {
+        @Override
+        public int arithemticCalculate(int operand1, int operand2) {
+            return operand1 - operand2;
+        }
+    }, MULTIPLICATION("*") {
+        @Override
+        public int arithemticCalculate(int operand1, int operand2) {
+            return operand1 * operand2;
+        }
+    }, DIVISION("/") {
+        @Override
+        public int arithemticCalculate(int operand1, int operand2) {
+            return operand1 / operand2;
+        }
+    };
+
+    private final String operator;
+
+    ArithmeticOperator(String operator) {
+        this.operator = operator;
+    }
+
+    public abstract int arithemticCalculate(final int operand1, final int operand2);
+
+    public static int calculate(int operand1, String operand, int operand2) {
+        ArithmeticOperator arithmeticOperator = Arrays.stream(values())
+                .filter(v -> v.operator.equals(operand))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("올바르지 않은 사칙연산입니다"));
+
+        return arithmeticOperator.arithemticCalculate(operand1, operand2);
+    }
+}
--- Calculator/src/main/java/org/example/Calculator.java
+++ Calculator/src/main/java/org/example/Calculator.java
@@ -2,15 +2,6 @@ package org.example;
 
 public class Calculator {
     public static int calculate(int operand1, String operand, int operand2) {
-        if("+".equals(operand)) {
-            return operand1 + operand2;
-        } else if ("-".equals(operand)) {
-            return operand1 - operand2;
-        } else if ("*".equals(operand)) {
-            return operand1 * operand2;
-        } else if ("/".equals(operand)) {
-            return operand1 / operand2;
-        }
-        return 0;
+        return ArithmeticOperator.calculate(operand1, operand, operand2);
     }
 }
