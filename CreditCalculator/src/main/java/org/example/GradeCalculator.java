88c27f037dbf2dc8547c82127019637d214a0daa
junha <goodjunha@gmail.com>
2023-02-16 20:05:51 +0900

    학점 평균 계산 기능추가

---
+++ CreditCalculator/src/main/java/org/example/GradeCalculator.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.util.List;
+
+public class GradeCalculator {
+
+    private final List<Course> coures;
+
+    public GradeCalculator(List<Course> courses) {
+        this.coures = courses;
+    }
+
+    public double calculateGrade() {
+        return 0;
+    }
+}
---
+++ CreditCalculator/src/test/java/org/example/GradeCalculatorTest.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class GradeCalculatorTest {
+    @Test
+    @DisplayName("평균학점을 계산한다")
+    void calculateGradeTest() {
+        List<Course> courses = List.of(new Course("OOP", 3, "A+"),
+                new Course("자료구조",3,"A++"));
+
+        GradeCalculator gradeCalculator = new GradeCalculator(courses);
+        double gradeResult = gradeCalculator.calculateGrade();
+
+        assertThat(gradeResult).isEqualTo(4.5);
+    }
+}
